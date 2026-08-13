795525b7bc9cee362e84ccbaa363a392f7890b2a
tomaszstaniak <68373859+tomaszstaniak@users.noreply.github.com>
2026-08-13 19:29:54 +0200

    parport: restore Boxer's parallel port and printer emulation
    Ports Boxer's LPT/printer emulation onto DOSBox Staging 0.84.0-alpha.
    Upstream still has no parport implementation (only hardware/lpt.h), so
    this remains Boxer-specific.
    
    Mechanical adaptations to the modern tree:
    - Bit8u/Bit16u/Bit32u/Bit8s/Bit16s replaced with stdint types (236
      sites); upstream keeps only Bitu/Bits/Real64 in misc/types.h
    - header renames: inout.h -> hardware/port.h, control.h ->
      config/config.h, dos_inc.h -> dos/dos.h, regs.h -> cpu/registers.h,
      timer.h -> hardware/timer.h
    - parport.h moved from the removed include/ into src/hardware/parport/
    
    Still to resolve in parport.cpp: OpenCaptureFile -> CAPTURE_CreateFile,
    DOS_Device::Close() bool -> void, BIOS_SetLPTPort, Section_prop ->
    SectionProp / Module_base removal.

---
+++ src/hardware/parport/parport.h
@@ -0,0 +1,109 @@
+/*
+ *  Copyright (C) 2002-2006  The DOSBox Team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef DOSBOX_PARPORT_H
+#define DOSBOX_PARPORT_H
+
+// set to 1 for debug messages and debugging log:
+#define PARALLEL_DEBUG 0
+
+#ifndef DOSBOX_DOSBOX_H
+#include "dosbox.h"
+#endif
+#ifndef DOSBOX_INOUT_H
+#include "hardware/port.h"
+#endif
+
+#include "config/config.h"
+#include "dos/dos.h"
+
+class device_LPT : public DOS_Device {
+public:
+	// Creates a LPT device that communicates with the num-th parallel port, i.e. is LPTnum
+	device_LPT(uint8_t num, class CParallel* pp);
+	~device_LPT();
+	bool Read(uint8_t * data,uint16_t * size);
+	bool Write(uint8_t * data,uint16_t * size);
+	bool Seek(uint32_t * pos,uint32_t type);
+	bool Close();
+	uint16_t GetInformation(void);
+private:
+	CParallel* pportclass;
+	uint8_t num; // This device is LPTnum
+};
+
+
+class CParallel {
+public:
+#if PARALLEL_DEBUG
+	FILE * debugfp;
+	bool dbg_data;
+	bool dbg_putchar;
+	bool dbg_cregs;
+	bool dbg_plainputchar;
+	bool dbg_plaindr;
+	void log_par(bool active, char const* format,...);
+#endif
+
+	// Constructor
+	CParallel(CommandLine* cmd, Bitu portnr, uint8_t initirq);
+	
+	virtual ~CParallel();
+
+	IO_ReadHandleObject ReadHandler[3];
+	IO_WriteHandleObject WriteHandler[3];
+
+	void setEvent(uint16_t type, float duration);
+	void removeEvent(uint16_t type);
+	void handleEvent(uint16_t type);
+	virtual void handleUpperEvent(uint16_t type)=0;
+	
+	Bitu port_nr;
+	Bitu base;
+	Bitu irq;
+	
+	// read data line register
+	virtual Bitu Read_PR()=0;
+	virtual Bitu Read_COM()=0;
+	virtual Bitu Read_SR()=0;
+
+	virtual void Write_PR(Bitu)=0;
+	virtual void Write_CON(Bitu)=0;
+	virtual void Write_IOSEL(Bitu)=0;
+
+	void Write_reserved(uint8_t data, uint8_t address);
+
+	virtual bool Putchar(uint8_t)=0;
+	bool Putchar_default(uint8_t);
+	uint8_t getPrinterStatus();
+	void initialize();
+	
+
+
+private:
+	 DOS_Device* mydosdevice;
+		
+};
+
+extern CParallel* parallelPortObjects[];
+void PARALLEL_Init (Section * sec);
+
+const uint16_t parallel_baseaddr[3] = {0x378,0x278,0x3bc};
+
+#endif
+
